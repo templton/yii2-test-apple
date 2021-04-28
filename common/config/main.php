ec37c926740e3df2050c699d9d86cba1fba09648
Salamatov <dimasalam@yandex.ru>
2021-04-28 10:35:46 +0300

    config db connection

--- common/config/main.php
+++ common/config/main.php
@@ -6,6 +6,13 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host='.env('MYSQL_HOST').';dbname='.env('MYSQL_DB_NAME'),
+            'username' => env('MYSQL_USER'),
+            'password' => env('MYSQL_PASSWORD'),
+            'charset' => 'utf8',
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
